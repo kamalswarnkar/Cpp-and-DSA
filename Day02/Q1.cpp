5d2752be6acb6014dfe1066ced5067febac219b1
Kamal <swarnkar889@gmail.com>
2024-08-26 14:36:46 +0530

    Q1.cpp

---
+++ Day02/Q1.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n1, n2, n3;
+    
+    cout << "Enter the first number: ";
+    cin >> n1;
+    
+    cout << "Enter the second number: ";
+    cin >> n2;
+    
+    cout << "Enter the third number: ";
+    cin >> n3;
+    
+    if(n1 == n2 == n3){
+        cout << "All 3 numbers are equal, hence no number is the largest";
+    }else if(n1 > n2 && n1 > n3){
+        cout << "The largest number is: " << n1;
+    }else if(n2 > n1 && n2 > n3){
+        cout << "The largest number is: " << n2;
+    }else{
+        cout << "The largest number is: " << n3;
+    }
+
+    return 0;
+}
