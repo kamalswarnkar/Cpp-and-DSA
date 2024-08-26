844d84a6be5f14811f6357dee1d479e2fd572710
Kamal <swarnkar889@gmail.com>
2024-08-26 15:01:42 +0530

    Q3.cpp

---
+++ Day01/Q3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+    int n, sq;
+    
+    cout << "Enter a number: ";
+    cin >> n;
+    
+    sq = n * n;
+    
+    cout << "The square of " << n << " is: " << sq;
+
+    return 0;
+}
