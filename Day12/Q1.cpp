64a5037f9d9c1f85794df27d3d02d100300be38a
Kamal <swarnkar889@gmail.com>
2024-09-15 18:06:59 +0530

    Q1.cpp

---
+++ Day12/Q1.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* next;
+};
+
+void printLinkedList(Node* head) {
+    Node* current = head;
+    while (current != nullptr) {
+        cout << current->data << " ";
+        current = current->next;
+    }
+    cout << endl;
+}
+
+int main() {
+    Node* head = new Node{10, nullptr};
+    head->next = new Node{20, nullptr};
+    head->next->next = new Node{30, nullptr};
+    head->next->next->next = new Node{40, nullptr};
+
+    printLinkedList(head);
+
+    return 0;
+}
